fdcbd7e9bed8d521498168dd4e2481c058c255a3
melanu1991 <melanu284733@gmail.com>
2017-08-15 19:58:25 +0300

    create project

---
+++ AnimationTask/AnimationTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTask
+//
+//  Created by melanu1991 on 15.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTask/AnimationTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTask
+//
+//  Created by melanu1991 on 15.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
